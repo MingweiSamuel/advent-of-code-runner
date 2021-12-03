874727f56f21cfc3b42627183c57f2d5de65ec34
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-12-03 10:57:36 -0800

    mingweisamuel Day 3 use bespoke BitVec

--- 2021/3/solutions/rust/mingweisamuel/src/main.rs
+++ 2021/3/solutions/rust/mingweisamuel/src/main.rs
@@ -1,9 +1,53 @@
-use bitvec::vec::BitVec;
 use std::io::BufRead;
 
-fn as_usize(bv: &BitVec) -> usize {
-    bv.iter().fold(0, |n, x| (n << 1) + (*x as usize))
+#[derive(Debug)]
+struct BitVec {
+    len: u8,
+    bits: usize,
 }
+impl BitVec {
+    pub fn new(len: usize, bits: usize) -> Self {
+        assert!((len as u32) < usize::BITS);
+        Self {
+            len: len as u8,
+            bits,
+        }
+    }
+    pub fn len(&self) -> usize {
+        self.len as usize
+    }
+    pub fn as_usize(&self) -> usize {
+        self.bits
+    }
+}
+impl std::ops::Index<usize> for BitVec {
+    type Output = bool;
+    fn index(&self, index: usize) -> &Self::Output {
+        assert!(index < self.len as usize);
+        let bit_idx = self.len - (index as u8) - 1;
+        match 0b1 == 0b1 & (self.bits >> bit_idx) {
+            true => &true,
+            false => &false,
+        }
+    }
+}
+impl std::iter::FromIterator<bool> for BitVec {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = bool>,
+    {
+        let mut len = 0;
+        let bits = iter
+            .into_iter()
+            .inspect(|_| {
+                len += 1;
+                assert!(len <= (usize::BITS as u8), "BitVec capacity exceeded");
+            })
+            .fold(0, |bits, bit| (bits << 1) + (bit as usize));
+        Self { len, bits }
+    }
+}
+
 fn common_bit(rows: &[&BitVec], i: usize) -> bool {
     let occ = rows.iter().fold(0, |occ, row| occ + (row[i] as usize));
     rows.len() <= 2 * occ
@@ -23,8 +67,7 @@ fn find_rating(mut rows: Vec<&BitVec>, common: bool) -> Option<&BitVec> {
     }
     if 1 < rows.len() {
         None
-    }
-    else {
+    } else {
         rows.pop()
     }
 }
@@ -34,14 +77,9 @@ fn main() {
         .lock()
         .lines()
         .map(|line| {
-            line.expect("Failed to read line as UTF-8.")
-                .chars()
-                .map(|c| match c {
-                    '0' => false,
-                    '1' => true,
-                    _ => panic!(),
-                })
-                .collect()
+            let line = line.expect("Failed to read line as UTF-8.");
+            let bits = usize::from_str_radix(&*line, 2).expect("Failed to parse binary integer.");
+            BitVec::new(line.len(), bits)
         })
         .collect();
 
@@ -51,23 +89,24 @@ fn main() {
     let commons: BitVec = rows
         .iter()
         .fold(vec![0_usize; width], |mut occ, row| {
-            occ.iter_mut()
-                .zip(row.iter())
-                .filter(|(_, b)| **b)
-                .for_each(|(s, _)| *s += 1);
+            for i in 0..width {
+                occ[i] += row[i] as usize;
+            }
             occ
         })
         .into_iter()
         .map(|occ| count <= 2 * occ)
         .collect();
 
-    let gamma = as_usize(&commons);
+    let gamma = commons.as_usize();
     let epsilon = ((0b1 << width) - 1) & (!gamma);
 
-    let generator = find_rating(rows.iter().collect(), true);
-    let generator = as_usize(generator.expect("Failed to determine generator rating."));
-    let scrubber = find_rating(rows.iter().collect(), false);
-    let scrubber = as_usize(scrubber.expect("Failed to determine scrubber rating."));
+    let generator = find_rating(rows.iter().collect(), true)
+        .expect("Failed to determine generator rating.")
+        .as_usize();
+    let scrubber = find_rating(rows.iter().collect(), false)
+        .expect("Failed to determine scrubber rating.")
+        .as_usize();
 
     println!("{}\n{}", gamma * epsilon, generator * scrubber);
 }
